7d1febf6137c0261e074229b039d6723d8692b62
arkxdesign <67521058+arkxdesign@users.noreply.github.com>
2024-07-24 20:09:31 -0600

    Entrega 1

---
+++ src/models/user.models.js
@@ -0,0 +1,37 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+    first_name: {
+        type: String,
+        required: true,
+    },
+    last_name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    age: {
+        type: Number,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: ["user", "admin"],
+        default: "user",
+    },
+    // cart: {
+    //     type: Schema.Types.ObjectId,
+    //     ref: "cart",
+    // }
+});
+
+export const userModel = model("user", userSchema);
+    
---
+++ src/server.js
@@ -0,0 +1,40 @@
+import express from "express";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import sessionRoutes from "./routes/session.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import morgan from "morgan";
+import { initializePassport } from "./config/passport.config.js";
+import passport from "passport";
+
+const app = express();
+const PORT = 8080; 
+
+// Express config
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static("public"));
+app.use(morgan("dev"));
+
+// Passport config
+initializePassport();
+app.use(passport.initialize());
+
+// Mongoose config
+mongoose
+    .connect("mongodb://127.0.0.1:27017/afterclass")
+    .then(() => {
+        console.log("Base de datos conectada a MongoDB", mongoose.connection.host, mongoose.connection.name);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+// Routes
+app.use("/api/sessions", sessionRoutes);
+app.use("/api/users", userRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto http://localhost:${PORT}`);
+})
